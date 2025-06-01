f95d489a6c61f10a871849e947fd2583866d9eaf
Radiat09 <rzsianboyridoy10@gmail.com>
2025-06-01 13:13:35 +0600

    install dotenv and update

--- dist/config/mongodb.js
+++ dist/config/mongodb.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = void 0;
 const mongodb_1 = require("mongodb");
-const uri = "mongodb+srv://L2FirstUser:OxlI0DauIjd66t0p@cluster0.fgalepw.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgalepw.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 exports.client = new mongodb_1.MongoClient(uri, {
     serverApi: {
